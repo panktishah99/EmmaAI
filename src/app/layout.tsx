4702353665505a79b1b3d924394dcdb3563a4588
panktishah99 <pankti99@uw.edu>
2024-11-02 10:23:13 -0700

    initial shadCN nextjs project

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next';
+
+import { Plus_Jakarta_Sans, Inter } from 'next/font/google';
+import '@/common/styles/globals.css';
+
+const plus_jakarta_sans = Plus_Jakarta_Sans({ subsets: ['latin'], preload: true });
+const inter = Inter({ subsets: ['latin'], preload: true });
+
+export const metadata: Metadata = {
+  title: 'APRIORA',
+  description: 'Coding Challenge',
+};
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${plus_jakarta_sans.className} ${inter.className}`} suppressHydrationWarning={true}>
+        {children}
+      </body>
+    </html>
+  );
+}
