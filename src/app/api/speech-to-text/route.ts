6f7e61143984109fa67b79504c9a88e6d0a45ebe
panktishah99 <pankti99@uw.edu>
2024-11-03 01:28:25 -0700

    removed test routes

--- src/app/api/speech-to-text/route.ts
+++
@@ -1,8 +0,0 @@
-import { speechToText } from '@/services/speech-to-text';
-import fs from 'fs';
-
-export async function GET(request: Request) {
-  const readStream = fs.readFileSync('output.wav');
-  const response = await speechToText(readStream);
-  return new Response(response);
-}
